321f8f08fe706c2563bb87ec889bd257000ef723
aamonnlimaa@gmail.com <aamonnlimaa@gmail.com>
2025-02-09 22:35:40 -0300

    feat: integrate Pinia into Vue app initialization

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { pinia } from '@/stores';
+
+import vuetify from './plugins/vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
 
 //icons
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/styles';
 
-const app = createApp(App)
-app.use(router)
-app.use(vuetify)
-app.mount('#app')
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+app.use(vuetify);
+app.mount('#app');
