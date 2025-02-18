bbf2f4bab4a201215243c45d18a837925dbf36cd
aamonnlimaa@gmail.com <aamonnlimaa@gmail.com>
2025-02-18 16:03:00 -0300

    feat: add routes for individual and all user notes

--- src/router/index.js
+++ src/router/index.js
@@ -1,56 +1,61 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import api from '@/services/api'
-import HomeView from '@/views/HomeView.vue'
-import HealthCheck from '@/views/HealthCheck.vue'
-import DashboardView from '@/views/DashboardView.vue'
-import LoginView from '@/views/LoginView.vue'
-import RegisterView from '@/views/RegisterView.vue'
-import CreateNoteView from '@/views/CreateNoteView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import api from "@/services/api";
+import HomeView from "@/views/HomeView.vue";
+import HealthCheck from "@/views/HealthCheck.vue";
+import LoginView from "@/views/LoginView.vue";
+import RegisterView from "@/views/RegisterView.vue";
+import CreateNoteView from "@/views/CreateNoteView.vue";
+import NoteView from "@/views/NoteView.vue";
+import AllNotesView from "@/views/AllNotesView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: HomeView,
-    meta: { requiresAuth: true }
+    meta: { requiresAuth: true },
   },
   {
-    path: '/health',
-    name: 'health',
+    path: "/health",
+    name: "health",
     component: HealthCheck,
   },
   {
-    path: '/dashboards',
-    name: 'dashboards',
-    component: DashboardView,
-    meta: { requiresAuth: true }
+    path: "/signin",
+    name: "signin",
+    component: LoginView,
   },
   {
-    path: '/signin',
-    name: 'signin',
-    component: LoginView
+    path: "/signup",
+    name: "signup",
+    component: RegisterView,
   },
   {
-    path: '/signup',
-    name: 'signup',
-    component: RegisterView
+    path: "/create-note",
+    name: "create-note",
+    component: CreateNoteView,
   },
   {
-    path: '/create-note',
-    name: 'create-note',
-    component: CreateNoteView
-  }
-]
+    path: "/users/notes/:id",
+    name: "note",
+    component: NoteView,
+  },
+  {
+    path: "/users/notes",
+    name: "all-notes",
+    component: AllNotesView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
 // Função para verificar autenticação
 async function checkAuth() {
   try {
-    const response = await api.get('users/welcome');
+    const response = await api.get("users/welcome");
     return response.status === 200;
   } catch (error) {
     return false;
@@ -63,18 +68,19 @@ router.beforeEach(async (to, from, next) => {
 
   if (to.meta.requiresAuth && !isAuthenticated) {
     next({
-      path: '/signin',
-      query: { 
-        login_needed: true
-      }
+      path: "/signin",
+      query: {
+        login_needed: true,
+      },
     });
-  } 
-  else if ((to.path === '/signin' || to.path === '/signup' ) && isAuthenticated) {
-    next('/');
-  }
-  else {
+  } else if (
+    (to.path === "/signin" || to.path === "/signup") &&
+    isAuthenticated
+  ) {
+    next("/");
+  } else {
     next();
   }
 });
 
-export default router
+export default router;
